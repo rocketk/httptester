bb4d4d83f31e1de87bcfd66c67de3182e1f6777f
pengyu <pengy70@chinaunicom.cn>
2021-03-12 15:19:00 +0800

    add support for apple m1

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 env GOOS=linux GOARCH=amd64 go build -o httptester-linux-amd64
 env GOOS=darwin GOARCH=amd64 go build -o httptester-darwin-amd64
+env GOOS=darwin GOARCH=arm64 go build -o httptester-darwin-arm64
 env GOOS=windows GOARCH=amd64 go build -o httptester-windows-amd64
